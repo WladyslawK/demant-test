13f385dff98d15dc1d65b7372584d2a044d9b73f
WladyslawK <vladkurr7890@gmail.com>
2023-01-15 20:11:27 +0100

    feat(initial Preparing)

---
+++ src/components/test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Test = () => {
+  return (
+    <div style={{width: '1440px', height: "20px", backgroundColor: 'blue'}}>
+
+    </div>
+  );
+};
+
+export default Test;
